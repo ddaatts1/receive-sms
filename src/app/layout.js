6b34a419baad1e5b85f7d585b1045fd49d7a0498
ddaatts1 <dodat743@gmail.com>
2025-11-17 16:39:31 +0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/layout/Header'
 import Footer from '@/components/layout/Footer'
+import Script from "next/script";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -36,7 +37,7 @@ export default function RootLayout({ children }) {
               gtag('config', 'G-FX4XWKJX2W');
             `}
         </Script>
-        
+
 
         </body>
         </html>
